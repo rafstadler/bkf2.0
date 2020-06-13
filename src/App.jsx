5f5bb361e02954b5484d7bad5029fd58a1e25892
raphael stadler <raph.stadler@gmail.com>
2020-06-13 12:11:29 +0200

    test insta mobile

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route ,withRouter , Link} from 'react-router-dom';
 import {Container, Row, Col, Navbar, NavDropdown} from 'react-bootstrap';
 import {TransitionGroup,CSSTransition} from "react-transition-group";
+import {BrowserView,MobileView,isBrowser,isMobile} from "react-device-detect";
 import Home from './Components/Home/Home';
 import WAW from './Components/WAW/WAW';
 import Works from './Components/Works/Works';
@@ -36,7 +37,7 @@ class App extends Component {
                         <div>
 
                             <Navbar className='color-nav' fixed='top' align='center'>
-                                
+
 
                                 <Col sm={2} className='d-none d-sm-none d-lg-block'><Link to='/' className='nav-brand'> Breakfast </Link></Col>
                                 <Col sm={8} className='d-lg-none d-sm-block'><Link to='/' className='nav-brand'> Breakfast </Link></Col>
@@ -44,16 +45,19 @@ class App extends Component {
                                 <Col sm={2} className='d-none d-sm-none d-lg-block'><Link to='/works#clothes' className='nav-link' style={this.state.page.pathname==="/works" ? {'text-decoration':'line-through underline'} : {}}> Works </Link></Col>
                                 <Col sm={2} className='d-none d-sm-none d-lg-block'><Link to='/exhib' className='nav-link' style={this.state.page.pathname==="/exhib" ? {'text-decoration':'line-through underline'} : {}}> Exhibitions </Link></Col>
                                 <Col sm={2} className='d-none d-sm-none d-lg-block'><Link to='/contact' className='nav-link' style={this.state.page.pathname==="/contact" ? {'text-decoration':'line-through underline'} : {}}> Contacts </Link></Col>
-                                <Col sm={2} className='d-none d-sm-none d-lg-block'><a target="_blank" rel="noopener noreferrer" href='https://www.instagram.com/breakfast_paris/' className='nav-link'> Instagram </a></Col>
+                                <Col sm={2} className='d-none d-sm-none d-lg-block'>
+                                    {isBrowser ? <a target="_blank" rel="noopener noreferrer" href='https://www.instagram.com/breakfast_paris/' className='nav-link' align='center' >Instagram</a> : <a href='instagram://user?username=breakfast_paris' className='nav-link' align='center' style={{'color':'black'}}>Instagram</a>}
+                                </Col>
 
 
                                 <Col sm={2} className="header-drop d-lg-none d-sm-block">
                                     <NavDropdown alignRight title={this.props.page} id="nav-dropdown">
-                                        <NavDropdown.Item><Link to='/who_are_we' className='nav-link' style={this.state.page.pathname==="/who_are_we" ? {'text-decoration':'line-through underline','color':'black'} : {'color':'black'}}>Who Are We</Link></NavDropdown.Item>
-                                        <NavDropdown.Item><Link to='/works#clothes' className='nav-link' style={this.state.page.pathname==="/works" ? {'text-decoration':'line-through underline','color':'black'} : {'color':'black'}}>Works</Link></NavDropdown.Item>
-                                        <NavDropdown.Item><Link to='/exhib' className='nav-link' style={this.state.page.pathname==="/exhib" ? {'text-decoration':'line-through underline','color':'black'} : {'color':'black'}}>Exhibitions</Link></NavDropdown.Item>
-                                        <NavDropdown.Item><Link to='/contact' className='nav-link' style={this.state.page.pathname==="/contact" ? {'text-decoration':'line-through underline','color':'black'} : {'color':'black'}}>Contacts</Link></NavDropdown.Item>
-                                        <NavDropdown.Item><Link to='https://www.instagram.com/breakfast_paris/' className='nav-link' style={{'color':'black'}}>Instagram</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to='/who_are_we' className='nav-link' align='center' style={this.state.page.pathname==="/who_are_we" ? {'text-decoration':'line-through underline','color':'black'} : {'color':'black'}}>Who Are We</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to='/works#clothes' className='nav-link' align='center' style={this.state.page.pathname==="/works" ? {'text-decoration':'line-through underline','color':'black'} : {'color':'black'}}>Works</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to='/exhib' className='nav-link'  align='center' style={this.state.page.pathname==="/exhib" ? {'text-decoration':'line-through underline','color':'black'} : {'color':'black'}}>Exhibitions</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to='/contact' className='nav-link' align='center' style={this.state.page.pathname==="/contact" ? {'text-decoration':'line-through underline','color':'black'} : {'color':'black'}}>Contacts</Link></NavDropdown.Item>
+                                        {isBrowser ? <a target="_blank" rel="noopener noreferrer" href='https://www.instagram.com/breakfast_paris/' className='nav-link' align='center' style={{'color':'black'}}>Instagram</a> : <a href='instagram://user?username=breakfast_paris' className='nav-link' align='center' style={{'color':'black'}}>Instagram</a>}
+                                        
 
                                     </NavDropdown>
                                 </Col>
